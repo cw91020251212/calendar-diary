1d39c9dc31e62a82961f1790e332d5947e2bfe67
cw91020251212 <cw9102025@gmail.com>
2026-06-12 00:47:25 +0800

    Add files via upload

---
+++ docs/dist/sw.js
@@ -0,0 +1,7 @@
+// Minimal service worker placeholder
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
